509cb8ab9dcfadaec44dc95bbf52b390a8bb3684
Amanda Kays <amandakays89@gmail.com>
2021-11-04 02:21:45 -0500

    homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,21 @@
 import React from "react"
+import image from "../assets/backgroundpink.jpg"
+
 
 export default function Home() {
-    return <h1>Home Page</h1>
+    return (
+        <main>
+            <img src={image} alt="Keyboard" className="absolute object-cover w-full h-full"/>
+            <section className="relative min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-4xl text-gray-500 leading-none lg:leading-snug flex justify-center font-bold ">Welcome To The Portfolio Of
+                </h1>
+                <h1 className="text-5xl text-gray-500 leading-none lg:leading-snug flex justify-center font-bold "> Amanda Kays
+                </h1>
+                <div>
+                <p className="text-3xl text-gray-500 flex justify-center m-5 font-bold">
+                 Full Stack Web Developer</p>
+                </div>
+            </section>
+        </main> 
+    )
 }
