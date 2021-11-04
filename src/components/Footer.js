75dae055ea07ee086dc936860c6605f2ddb91b4f
Amanda Kays <amandakays89@gmail.com>
2021-11-04 01:20:22 -0500

    Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+
+import React from "react"
+
+export default function Footer(){
+    return(
+        <div className="rounded-lg text-center text-gray-500 bg-white" >
+            <a href="https://github.com/akays89/reactPortfolio">&copy; akays89 2021 </a>
+        </div>
+    )
+} 
