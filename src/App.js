42d4d0e597bd69d369d7fbc9769bdb632c14051d
Amanda Kays <amandakays89@gmail.com>
2021-11-04 02:23:16 -0500

    React router dom had replaced switch with routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Routes  } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Project from "./components/Project";
@@ -10,12 +10,12 @@ function App() {
   return (
     <BrowserRouter>
     <NavBar />
-    <Switch>
+    <Routes>
       <Route component={Home} path="/" exact />
       <Route component={About} path="/about" />
       <Route component={Project} path="/project" />
       <Route component={Contact} path="/contact" />
-    </Switch>
+    </Routes>
     <Footer />
   </BrowserRouter>
   );
