3ebe488740e926733a00b3cb8542810138623c7d
Amanda Kays <amandakays89@gmail.com>
2021-11-05 01:56:13 -0500

    ,

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch  } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Project from "./components/Project";
